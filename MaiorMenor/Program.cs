ff09cc18b90a9a04c80c8d8336ad7852f12673de
FilippiMnz <imperilkill12@gmail.com>
2022-12-10 16:47:00 -0300

    Kenpachi

---
+++ MaiorMenor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Mais
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 numeros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O maior numero é " + resultado);
+
+            /*
+                        if (n1 > n2 && n1 > n3)
+                        {
+                            Console.WriteLine("O maior numero é " + n1);
+                        }
+                        else if (n2 >n3)
+                        {
+                            Console.WriteLine("O maior numero é " + n2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("O maior numero é " + n3);
+                        }
+
+                    }
+            */
+            static int Maior(int a, int b, int c)
+            {
+                int m;
+                if (a > b && a > c)
+                {
+                    m = a;
+                }
+                else if (b> c) {
+                    m = b;
+                }
+                else
+                {
+                    m = c;
+                }
+                return m;
+            }
+
+
+
+        }
+
+    }
+}
+
+
+    
+
+   
